03560d940610dee5781a875f2c794196641cba7d
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2013-02-06 15:24:13 +0100

    Leagues displayer implementation

--- displayCompetition.py
+++ displayCompetition.py
@@ -1,16 +1,110 @@
 import os
+import webbrowser
+from HTMLParser import HTMLParser
+
+class RoundDisplayHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print "Encountered a start tag:", tag
+    def handle_endtag(self, tag):
+        print "Encountered an end tag :", tag
+    def handle_data(self, data):
+        print "Encountered some data  :", data
 
 class DisplayCompetition(object):
-    submissionsDir = 'submissions'
+    submissionsDir = 'submissions/'
+    tournamentResultsDir = 'tournamentResults/'
+    leaguesDir = tournamentResultsDir + '0/'
+    leagueDisplayFile = 'displayLeague.html'
+    quarterFinalsRound = 1
+    semiFinalsRound = 2
+    finalRound = 4
+    numberOfLeagues = 4
 
     groups2Bots = {}
+    leaguesMaps = []
     
     def __init__(self):
         self.initGroups2Bots()
+        self.doLeagues()
+        self.displayLeagues()
 
     def initGroups2Bots(self):
         for d in os.listdir(self.submissionsDir):
-            f = open(self.submissionsDir + '/' + d + '/bot.txt', 'r')
+            f = open(self.submissionsDir + d + '/bot.txt', 'r')
             self.groups2Bots[d] = f.readline()
+
+    def doLeagues(self):
+        for league in range(1, self.numberOfLeagues + 1):
+            for match in os.listdir(self.leaguesDir + str(league)):
+                print "Entering match {}".format(match)
+                playerA = match.split('-')[0]
+                playerAWins = 0
+                playerB = match.split('-')[1]
+                playerBWins = 0
+                matchDir = self.leaguesDir + str(league) + '/' + match
+                for rnd in os.listdir(matchDir):
+                    if rnd.split('.')[-1] != "txt":
+                        continue
+                    winner = rnd.split('_')[0][1:]
+                    if winner == playerA:
+                        playerAWins += 1
+                    elif winner == playerB:
+                        playerBWins += 1
+                    else:
+                        playerAWins += 1
+                        playerBWins += 1
+                matchWinner = ''
+                matchLooser = ''
+                if playerAWins > playerBWins:
+                    matchWinner = playerA
+                    matchLooser = playerB
+                elif playerBWins > playerAWins:
+                    matchWinner = playerB
+                    matchLooser = playerA
+                else:
+                    print "Draw game!"
+                    matchWinner = playerA
+                    matchLooser = playerB
+                print "Group {} wins against group {}".format(matchWinner, matchLooser)
+                wonRoundFiles = [rnd for rnd in os.listdir(matchDir) if rnd.split('_')[0] == 'w' + matchWinner and rnd.split('.')[-1] != 'txt']
+                maxSizeRoundFiles = [f for f in wonRoundFiles if f.endswith('map5')]
+                if len(maxSizeRoundFiles) == 0:
+                    maxSizeRoundFiles = [f for f in wonRoundFiles if f.endswith('map4')]
+                if len(maxSizeRoundFiles) == 0:
+                    maxSizeRoundFiles = [f for f in wonRoundFiles if f.endswith('map3')]
+                if len(maxSizeRoundFiles) == 0:
+                    maxSizeRoundFiles = [f for f in wonRoundFiles if f.endswith('map2')]
+                #print maxSizeRoundFiles
+                maxSizeRoundFile = sorted(maxSizeRoundFiles)[-1]
+                self.leaguesMaps.append(maxSizeRoundFile)
+
+    def displayLeagues(self):
+                #roundDisplayParser = RoundDisplayHTMLParser()
+                #roundDisplayParser.feed()
+
+                htmlLeagueDisplayFile = open(self.leagueDisplayFile, 'w')
+                fileContents = """
+<html>
+  <head>
+    <title>
+      PlanetWars Tournament
+    </title>
+  </head>
+  <body>
+    <iframe src='tournamentResults/0/1/11-12/w11_12-11_map4/generated.htm' height=800 width=900 frameborder=0></iframe>
+    <iframe src='tournamentResults/0/1/11-18/w11_11-18_map2/generated.htm' height=800 width=900 frameborder=0></iframe>
+    <iframe src='tournamentResults/0/1/2-18/w18_18-2_map4/generated.htm' height=800 width=900 frameborder=0></iframe>
+    <iframe src='tournamentResults/0/1/5-18/w18_18-5_map2/generated.htm' height=800 width=900 frameborder=0></iframe>
+  </body>
+</html>
+"""
+                htmlLeagueDisplayFile.write(fileContents)
+                htmlLeagueDisplayFile.close()
+
+
+                #maxSizeRoundPath = os.path.dirname(os.path.realpath(__file__)) + '/' + matchDir + '/' + maxSizeRoundFile +  '/generated.htm'
+                #webbrowser.open("file://" + maxSizeRoundPath)
+                                
+                    
         
 dc = DisplayCompetition()
